078e8eabac4326c0ccf4375d86349b6d5d241e9b
abhishek.srivastava@ <abhishek.srivastava@neudesic.com>
2023-06-27 10:09:30 +0530

    comemnts are removed

--- server.js
+++ server.js
@@ -1,24 +1,14 @@
-/*
 const express = require("express");
 const app = require("./app");
 require("dotenv").config();
+const config = require(process.env.CONFIG_PATH);
 
-app.listen(config.server.port, config.server.host, (err) => {
+const server = app.listen(config.server.port, config.server.host, (err) => {
   if (err) {
     console.log(err);
     process.exit(1);
   }
   console.log(
-    `Server is running on ${config.server.port}: ${server.address().port}`
+    `Server is running on ${config.server.host}: ${server.address().port}`
   );
-});
-*/
-const express = require("express");
-const app = require("./app");
-
-const PORT = 3000;
-
-// Start the server
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
 });
